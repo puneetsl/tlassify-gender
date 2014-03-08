6342789b17780cd39d04a5008de6a19af85e0c8d
puneetsl <everlasting.puneet@gmail.com>
2014-03-08 05:14:12 -0500

    Commit before sleep

--- src/com/pyrobits/tlassify/gender/main/MainTest.java
+++ src/com/pyrobits/tlassify/gender/main/MainTest.java
@@ -11,12 +11,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.pyrobits.tlassify.gender.loader.NameDictionaryLoader;
-
 public class MainTest {
+	public static String cleanName(String input)
+	{
+		return input.replaceAll("(.)\\1{2,}+","$1");
+	}
+
 	public static void main(String[] args) throws IOException, JSONException {
 		//InputStream input = null;
 		NameDictionaryLoader udl = new NameDictionaryLoader();
-		System.out.println(udl.getFemaleNameIndex("sofia"));
+//		System.out.println(udl.getFemaleNameIndex("Maria"));
 		BufferedReader br = new BufferedReader(new FileReader("userdata/gender.user.json"));
 		String line;
 		String str = "";
@@ -36,52 +40,80 @@ public class MainTest {
 			JSONObject temp= obj.getJSONObject(tmpKey);
 			if(temp.has("Name"))
 			{
-				String Name = temp.getString("Name");
+				String Name = temp.getString("Name").replaceAll("[^a-zA-Z ]", " ");
+				String ScreenName = temp.getString("Screen_Name").replaceAll("[^a-zA-Z ]", " ");
+				Name = Name.replaceAll("([a-z])([A-Z])", "$1 $2");//CamelCaseRemove
+				Name = cleanName(Name);
+				ScreenName = ScreenName.replaceAll("([a-z])([A-Z])", "$1 $2");//CamelCaseRemove
+				ScreenName = cleanName(ScreenName);
+				String Bio = temp.getString("Bio").replaceAll("[^a-zA-Z ]", " ");
+				Bio = cleanName(Bio);
 				String Label = temp.getString("Label");
 				String myLabel="";
-				
+				Name = Name +" " +ScreenName;
+				Name =Name +" "+ Bio;
 				String nameTokens[] = Name.split(" ");
 				float maleProb=0,femaleProb=0;
+				float i=4f;
+				int k=1;
 				for (String nameToken : nameTokens) {
+					i=i-0.2f;//giving weight to first name
 					if(udl.getMaleNameIndex(nameToken) !=0)
-						maleProb += (1000-udl.getMaleNameIndex(nameToken));
+						maleProb += ((1000-udl.getMaleNameIndex(nameToken))/1000f)*(i+k);
 					if(udl.getFemaleNameIndex(nameToken) !=0)
-						femaleProb += (1000-udl.getFemaleNameIndex(nameToken));
+						femaleProb += ((1000-udl.getFemaleNameIndex(nameToken))/1000f)*(i+k);
+					k=0;
 				}
+				
 				if(maleProb>femaleProb)
 				{
+					System.out.println(maleProb+"=>"+femaleProb);
 					myLabel = "male"; 
 				}
 				else if(maleProb<femaleProb)
 				{
 					myLabel = "female";
 				}
+				else
+				{
+					
+//					System.out.println(Name);
+				}
 				if(Label.equals("male"))
 				{
 					totalMale++;
+//					System.out.println(Bio);
 					if(myLabel.equals("male"))
 						rightMale++;
 					if(myLabel.equals("female"))
+					{
 						wrongMale++;
+//						System.out.println("wrongly classified as female:"+Name);
+					}
+					
 				}
 				if(Label.equals("female"))
 				{
 					totalFemale++;
+//					System.out.println(Bio);	
 					if(myLabel.equals("female"))
 						rightFemale++;
 					if(myLabel.equals("male"))
+					{
 						wrongFemale++;
+//						System.out.println("wrongly classified as male:"+Name);
+					}
 				}
 				
 			}
 		}
 		System.out.println("Total Male:"+totalMale);
 		System.out.println("Right Male:"+rightMale);
-		System.out.println("Wrong Male:"+wrongMale);
+		System.out.println("wrongly classified as female:"+wrongMale);
 		System.out.println("Not Classified Male:"+(totalMale-wrongMale-rightMale));
 		System.out.println("Total Female:"+totalFemale);
 		System.out.println("Right Female:"+rightFemale);
-		System.out.println("Wrong Female:"+wrongFemale);
+		System.out.println("wrongly classified as male:"+wrongFemale);
 		System.out.println("Not Classified Female:"+(totalFemale-wrongFemale-rightFemale));
 	}
 }
